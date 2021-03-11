e747ada3cac2cdae35e4c8c443f1f075e3832aa1
TCM.CustomRoom <51810037+rolnzaza@users.noreply.github.com>
2021-03-11 23:22:07 +0700

    Delete tree_del.php

--- tree_del.php
+++
@@ -1,9 +0,0 @@
-<h1>tree del</h1>
-<?php include("assets/connect.php");?>
-<?php
-$sql="delete from tree
-	where tree_id = '$_GET[id]'
-	";
-	mysqli_query($con,$sql)or die(mysqli_error($con));
-	header("Location:?page=tree");
-?>
